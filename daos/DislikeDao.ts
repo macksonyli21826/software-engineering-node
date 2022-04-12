f1df540c9d40f1964fbeef705bc1080e5d6c4d3b
macksonyqli <li.yuanqia@northeastern.edu>
2022-04-12 16:20:25 -0500

    first commit on dislikes button

---
+++ daos/DislikeDao.ts
@@ -0,0 +1,46 @@
+import DislikeDaoI from "../interfaces/DislikeDaoI";
+import DislikeModel from "../mongoose/dislikes/DislikeModel";
+import Dislike from "../models/dislikes/Dislike";
+export default class DislikeDao implements DislikeDaoI {
+    private static dislikeDao: DislikeDao | null = null;
+    public static getInstance = (): DislikeDao => {
+        if(DislikeDao.dislikeDao === null) {
+            DislikeDao.dislikeDao = new DislikeDao();
+        }
+        return DislikeDao.dislikeDao;
+    }
+    private constructor() {}
+
+    // This function checks if there's a likes document in the database for user/tuit combination
+    findUserDislikesTuit = async (uid: string, tid: string): Promise<any> =>
+        DislikeModel.findOne({tuit: tid, dislikedBy: uid});
+
+    // count how many users liked a tuit
+    countHowManyDislikedTuit = async (tid: string): Promise<any> =>
+        DislikeModel.count({tuit: tid});
+
+    // insert document into likes collection to record that user uid likes tuit tid
+    userDislikesTuit = async (uid: string, tid: string): Promise<any> =>
+        DislikeModel.create({tuit: tid, dislikedBy: uid});
+
+    // delete document from likes collection to record that user uid no longer likes tuit tid
+    userUndislikesTuit = async (uid: string, tid: string): Promise<any> =>
+        DislikeModel.deleteOne({tuit: tid, dislikedBy: uid});
+
+    findAllUsersThatDislikedTuit = async (tid: string): Promise<Dislike[]> =>
+        DislikeModel
+            .find({tuit: tid})
+            .populate("dislikedBy")
+            .exec();
+    findAllTuitsDislikedByUser = async (uid: string): Promise<Dislike[]> =>
+        DislikeModel
+            .find({dislikedBy: uid})
+            .populate({
+                path: "tuit",
+                populate: {
+                    path: "postedBy"
+                }
+            })
+            .exec();
+
+}
---
+++ mongoose/dislikes/DislikeSchema.ts
@@ -0,0 +1,9 @@
+import mongoose, {Schema} from "mongoose";
+import Dislike from "../../models/dislikes/Dislike";
+
+
+const DislikeSchema = new mongoose.Schema<Dislike>({
+    tuit: {type: Schema.Types.ObjectId, ref: "TuitModel"},
+    DislikedBy: {type: Schema.Types.ObjectId, ref: "UserModel"},
+}, {collection: "dislikes"});
+export default DislikeSchema;
